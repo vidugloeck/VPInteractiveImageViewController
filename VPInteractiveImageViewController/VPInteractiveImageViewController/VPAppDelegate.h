9758f15a46b74113397803ccd8616ea1b99f83cd
Vidu Pirathaparajah <writetovidu@gmail.com>
2014-01-27 21:59:54 +0100

    Add barebone project

---
+++ VPInteractiveImageViewController/VPInteractiveImageViewController/VPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VPAppDelegate.h
+//  VPInteractiveImageViewController
+//
+//  Created by Vidu Pirathaparajah on 27/01/14.
+//  Copyright (c) 2014 Vidu Pirathaparajah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VPInteractiveImageViewController/VPInteractiveImageViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VPInteractiveImageViewController
+//
+//  Created by Vidu Pirathaparajah on 27/01/14.
+//  Copyright (c) 2014 Vidu Pirathaparajah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VPAppDelegate class]));
+    }
+}
