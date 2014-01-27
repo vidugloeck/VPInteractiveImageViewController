38aa2bb971b6b7379a6a03cb3f9849f35d548561
Vidu Pirathaparajah <writetovidu@gmail.com>
2014-01-27 23:30:56 +0100

    Add VPInteractiveImageView

---
+++ VPInteractiveImageViewController/VPInteractiveImageViewController/VPInteractiveImageView.m
@@ -0,0 +1,28 @@
+//
+//  VPInteractiveImageView.m
+//  VPInteractiveImageViewController
+//
+//  Created by Vidu Pirathaparajah on 27/01/14.
+//  Copyright (c) 2014 Vidu Pirathaparajah. All rights reserved.
+//
+
+#import "VPInteractiveImageView.h"
+
+@implementation VPInteractiveImageView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        UITapGestureRecognizer *recognizer = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                                     action:@selector(imageViewTapped:)];
+        [self addGestureRecognizer:recognizer];
+    }
+    return self;
+}
+
+- (void)imageViewTapped:(UITapGestureRecognizer *)recognizer {
+    //TODO: Present InteractiveViewController
+}
+
+@end
