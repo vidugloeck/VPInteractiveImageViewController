fdacb7aaf5a72cb2ae46458cab7a4c3aa15e2f16
Vidu Pirathaparajah <writetovidu@gmail.com>
2014-01-28 00:19:30 +0100

    Add VPInteractiveImageViewController

---
+++ VPInteractiveImageViewController/VPInteractiveImageViewController/VPInteractiveImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  VPInteractiveImageViewController.h
+//  VPInteractiveImageViewController
+//
+//  Created by Vidu Pirathaparajah on 27/01/14.
+//  Copyright (c) 2014 Vidu Pirathaparajah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VPInteractiveImageViewController : UIViewController
+@property (nonatomic) UIImage *image;
+@end
---
+++ VPInteractiveImageViewController/VPInteractiveImageViewController/VPInteractiveImageViewController.m
@@ -0,0 +1,41 @@
+//
+//  VPInteractiveImageViewController.m
+//  VPInteractiveImageViewController
+//
+//  Created by Vidu Pirathaparajah on 27/01/14.
+//  Copyright (c) 2014 Vidu Pirathaparajah. All rights reserved.
+//
+
+#import "VPInteractiveImageViewController.h"
+
+@interface VPInteractiveImageViewController ()
+@property (nonatomic) UIImageView *imageView;
+@end
+
+@implementation VPInteractiveImageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor blackColor];
+	self.imageView = [[UIImageView alloc] initWithImage:_image];
+    self.imageView.contentMode = UIViewContentModeScaleAspectFit;
+    [self.view addSubview:self.imageView];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    self.imageView.frame = self.view.bounds;
+}
+
+#pragma mark - Getter / Setter
+
+- (void)setImage:(UIImage *)image {
+    _image = image;
+    self.imageView.image = image;
+}
+
+@end
