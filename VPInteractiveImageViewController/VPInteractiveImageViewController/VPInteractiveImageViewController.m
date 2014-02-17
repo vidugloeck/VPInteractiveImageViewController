14309a3decf23ad6a2ca5c4804c6b6ae7af46a8e
Vidu Pirathaparajah <writetovidu@gmail.com>
2014-02-17 22:03:44 +0100

    Add zooming by using a scrollView in VPInteractiveImageVC

--- VPInteractiveImageViewController/VPInteractiveImageViewController/VPInteractiveImageViewController.m
+++ VPInteractiveImageViewController/VPInteractiveImageViewController/VPInteractiveImageViewController.m
@@ -8,8 +8,10 @@
 
 #import "VPInteractiveImageViewController.h"
 
-@interface VPInteractiveImageViewController ()
+@interface VPInteractiveImageViewController () <UIScrollViewDelegate>
 @property (nonatomic) UIImageView *imageView;
+@property (nonatomic) UIScrollView *scrollView;
+@property (nonatomic) UITapGestureRecognizer *tapRecognizer;
 @end
 
 @implementation VPInteractiveImageViewController
@@ -18,17 +20,29 @@
     self = [super init];
     if (self) {
         _imageView = [[UIImageView alloc] initWithFrame:CGRectZero];
-        UITapGestureRecognizer *recognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(viewTapped:)];
-        [self.view addGestureRecognizer:recognizer];
+        _imageView.contentMode = UIViewContentModeScaleAspectFit;
+        _tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(viewTapped:)];
+        [self.view addGestureRecognizer:_tapRecognizer];
     }
     return self;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    [self setupScrollView];
+
     self.view.backgroundColor = [UIColor blackColor];
-    self.imageView.contentMode = UIViewContentModeScaleAspectFit;
-    [self.view addSubview:self.imageView];
+    [self.scrollView addSubview:self.imageView];
+
+}
+
+- (void)setupScrollView {
+    self.scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    self.scrollView.delegate = self;
+    self.scrollView.maximumZoomScale = 2;
+    self.scrollView.autoresizingMask = self.view.autoresizingMask;
+    [self.view addSubview:self.scrollView];
 }
 
 - (void)viewWillLayoutSubviews {
@@ -38,6 +52,22 @@
 
 - (void)viewTapped:(UITapGestureRecognizer *)gestureRecognizer {
     [self.presentingViewController dismissViewControllerAnimated:YES completion:NULL];
+}
+
+#pragma mark - UIScrollViewDelegate
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    return self.imageView;
+}
+
+- (void)scrollViewWillBeginZooming:(UIScrollView *)scrollView withView:(UIView *)view {
+    self.tapRecognizer.enabled = NO;
+}
+
+- (void)scrollViewDidEndZooming:(UIScrollView *)scrollView withView:(UIView *)view atScale:(CGFloat)scale {
+    if (scale <= self.scrollView.minimumZoomScale) {
+        self.tapRecognizer.enabled = YES;
+    }
 }
 
 @end
