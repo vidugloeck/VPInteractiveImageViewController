bfa6900a52e2760569dcb13f2347541df1c5b7d6
Vidu Pirathaparajah <writetovidu@gmail.com>
2014-02-17 14:16:06 +0100

    Add pan to close gesture in VPTransitionInteractor

--- VPInteractiveImageViewController/VPInteractiveImageViewController/VPTransitionInteractor.m
+++ VPInteractiveImageViewController/VPInteractiveImageViewController/VPTransitionInteractor.m
@@ -41,6 +41,12 @@
     }
     self.pinchableView.userInteractionEnabled = YES;
     [self.pinchableView addGestureRecognizer:gestureRecognizer];
+
+    if (!self.viewController)
+        return;
+
+    UIPanGestureRecognizer *panGestureRecognizer = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(handlePanClose:)];
+    [self.pinchableView addGestureRecognizer:panGestureRecognizer];
 }
 
 - (void)handlePinchOpen:(UIPinchGestureRecognizer *)pinch {
@@ -98,6 +104,43 @@
             CGFloat percent = (1.0 - scale/_fixedScale);
             BOOL cancelled = ([pinch velocity] < 5.0 && percent <= 0.3);
 
+            if (cancelled) {
+                [self cancelInteractiveTransition];
+            } else {
+                [self finishInteractiveTransition];
+            }
+            self.isInteractiveTransition = NO;
+            break;
+        }
+        case UIGestureRecognizerStateCancelled:
+        case UIGestureRecognizerStateFailed:
+            [self cancelInteractiveTransition];
+            self.isInteractiveTransition = NO;
+            break;
+        case UIGestureRecognizerStatePossible:
+            break;
+    }
+}
+
+- (void)handlePanClose:(UIPanGestureRecognizer *)gestureRecognizer {
+    CGFloat translationY = [gestureRecognizer translationInView:self.pinchableView].y;
+    switch (gestureRecognizer.state) {
+        case UIGestureRecognizerStateBegan:
+            self.isInteractiveTransition = YES;
+            [self.viewController dismissViewControllerAnimated:YES
+                                                    completion:NULL];
+            break;
+        case UIGestureRecognizerStateChanged: {
+            CGFloat percent = ((1.0f/200) * translationY);
+            percent = (percent > 100) ? 100 : percent;
+            percent = (percent < 0) ? 0 : percent;
+            NSLog(@"%f percent for translation: %f",percent,translationY);
+            [self updateInteractiveTransition:percent];
+            break;
+        }
+        case UIGestureRecognizerStateEnded: {
+            CGFloat percent = (1.0 - translationY/200);
+            BOOL cancelled = ([gestureRecognizer velocityInView:self.pinchableView].y < 5.0 && percent <= 0.3);
             if (cancelled) {
                 [self cancelInteractiveTransition];
             } else {
